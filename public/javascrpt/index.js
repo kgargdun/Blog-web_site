84382d2d538359e49a2efb996816690dc7fc5804
Kaustubh Garg <kaustubhgarg@Kaustubhs-MacBook-Air.local>
2020-02-20 21:40:07 +0530

    Initial Commit

---
+++ public/javascrpt/index.js
@@ -0,0 +1,8 @@
+// $("#home").on("click",function(event)
+// { console.log(this.id); });
+//
+// $("#about").on("click",function(event)
+// { console.log(this.id); });
+//
+// $("#contact").on("click",function(event)
+// { console.log(this.id); });
